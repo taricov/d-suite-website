18cee5ad082ea2972a7efdb4132e073baf037b90
TaricOv <tareq.ouf@gmail.com>
2023-07-12 02:26:54 +0300

    added 404page

---
+++ src/pages/404.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <main class="flex flex-col gap-y-10">
+    <section class="">
+    <div class="text-[190px] text-slate-100 mx-auto font-bold w-full flex justify-center m-0">404</div>
+    <div class="container flex items-center px-6 py-1 mx-auto">
+        <div class="flex flex-col items-center max-w-sm mx-auto text-center">
+            <!-- <p class="p-3 text-sm font-medium text-blue-500 rounded-full bg-blue-50 dark:bg-gray-800"> -->
+                <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
+                </svg> -->
+            <!-- </p> -->
+            <h1 class="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">Page not found</h1>
+            <p class="mt-4 text-gray-500 dark:text-gray-400">The page you are looking for doesn't exist. Here are some helpful links:</p>
+
+            <div class="flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto">
+                <RouterLink to="/" class="w-1/2 px-5 py-2 text-sm flex space-x-2 tracking-wide  transition-colors duration-200 rounded-lg shrink-0 sm:w-auto bc">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 rtl:rotate-180">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                    </svg>
+
+                    <span>
+                        Take me home
+                    </span>
+                </RouterLink>
+
+            </div>
+        </div>
+    </div>
+</section>
+  </main>
+</template>
+<style>
+.tc{
+@apply bg-gradient-to-bl from-[#4a3c71] to-[#afa6c1]
+}
+.bc{
+@apply bg-[#4a3c71] hover:bg-[#4a3c71]/80
+}
+
+
+</style>
