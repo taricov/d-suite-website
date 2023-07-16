1f3c2f7db7010f7b0f210dae859e30367554e7e2
TaricOv <tareq.ouf@gmail.com>
2023-07-16 23:14:16 +0300

    finished the dnotes page

--- src/pages/products/d-notes.vue
+++ src/pages/products/d-notes.vue
@@ -9,89 +9,98 @@ definePageMeta({
         <svg width="150" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128.65 139.81"><defs></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="notes1" d="M94.85,5.15,0,139.81,52.44,0H64.19Q83.17,0,94.85,5.15Z"/><polygon class="notes2" points="52.44 0 0 139.81 0 0 52.44 0"/><path class="notes3" d="M109.39,124.89A46.32,46.32,0,0,1,89.93,136a100.26,100.26,0,0,1-25.74,3.81H0L126.83,49.42a106.53,106.53,0,0,1,1.82,20q0,22.41-5.1,34.76a57.54,57.54,0,0,1-14.16,20.7"/><path class="notes4" d="M126.83,49.42,0,139.81,94.85,5.15a50,50,0,0,1,19.31,14.78,61.41,61.41,0,0,1,11.06,22.42C125.85,44.67,126.38,47,126.83,49.42Z"/></g></g></svg>
     <div class="capitalize mx-auto text-[100px] font-bold text-[#28727b]">d-notes</div>
     </div>
+    <div class="text-center">
+        <a href="#to-theextension page" class="notes-btn mx-auto px-3 py-2 rounded duration-300 transition text-2xl">Go to the Extension Page</a>
+		</div>
 	<div class="">
 		<div class="px-40 py-16 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        
 			<div class="max-w-3xl mx-auto text-center">
 				<h2 class="text-3xl font-extrabold sm:text-4xl capitalize">take notes and keep organized!</h2>
-				<p class="mt-4 text-lg dark:text-gray-400">Pellentesque viverra, leo id euismod laoreet, nunc risus molestie orci, vel faucibus quam justo id mauris.</p>
+				<p class="mt-4 text-lg dark:text-gray-400">A Utility and Extension App for note-taking that works on top on top of <b>Daftra</b> ERP.</p>
 			</div>
 			<dl class="mt-12 space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 lg:grid-cols-4 lg:gap-x-8">
 				<div class="flex">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 dark:text-violet-400">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-[#168896]">
 						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
 					</svg>
 					<div class="ml-3">
-						<dt class="text-lg font-medium">Quisque </dt>
-						<dd class="mt-2 dark:text-gray-400">Vivamus ultricies bibendum tortor, molestie imperdiet justo cursus eu. Donec quis arcu magna. Integer tempor egestas dolor.</dd>
+						<dt class="text-lg font-medium">Easy Noting</dt>
+						<dd class="mt-2 dark:text-gray-400">Take notes as easy as it hadn't been before.</dd>
 					</div>
 				</div>
 				<div class="flex">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 dark:text-violet-400">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-[#168896]">
 						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
 					</svg>
 					<div class="ml-3">
-						<dt class="text-lg font-medium">Quisque eu dui lacinia</dt>
-						<dd class="mt-2 dark:text-gray-400">Quisque ultricies volutpat sapien nec varius. Sed sit amet justo vestibulum, efficitur risus quis, gravida libero.</dd>
+						<dt class="text-lg font-medium">Ready to connect</dt>
+						<dd class="mt-2 dark:text-gray-400">Plug and Play with no much integration overhead and the minimal configuration .</dd>
 					</div>
 				</div>
 				<div class="flex">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 dark:text-violet-400">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-[#168896]">
 						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
 					</svg>
 					<div class="ml-3">
-						<dt class="text-lg font-medium">Mauris dignissim</dt>
-						<dd class="mt-2 dark:text-gray-400">Phasellus nec molestie arcu. Proin dictum, lorem mollis rutrum efficitur, lectus velit pharetra elit, et dictum purus nibh auctor velit.</dd>
+						<dt class="text-lg font-medium">Per page</dt>
+						<dd class="mt-2 dark:text-gray-400">Keep your notes organized per page and where you need to refer back to them later.</dd>
 					</div>
 				</div>
 				<div class="flex">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 dark:text-violet-400">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-[#168896]">
 						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
 					</svg>
 					<div class="ml-3">
-						<dt class="text-lg font-medium">Proin nulla eros</dt>
-						<dd class="mt-2 dark:text-gray-400">Sed ornare ultricies gravida. Morbi egestas dolor turpis, ornare laoreet est vehicula non.</dd>
+						<dt class="text-lg font-medium">Speed fast</dt>
+						<dd class="mt-2 dark:text-gray-400">Powered by a couple of shortcuts that will make your speed fast.</dd>
 					</div>
 				</div>
 				<div class="flex">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 dark:text-violet-400">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-[#168896]">
 						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
 					</svg>
 					<div class="ml-3">
-						<dt class="text-lg font-medium">Proin dictum</dt>
-						<dd class="mt-2 dark:text-gray-400">Lorem mollis rutrum efficitur, lectus velit pharetra elit, et dictum purus nibh auctor velit.</dd>
+						<dt class="text-lg font-medium">Filter tooling</dt>
+						<dd class="mt-2 dark:text-gray-400">Use Filters from the Dashboard to reach what you need to reach with ease.</dd>
 					</div>
 				</div>
 				<div class="flex">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 dark:text-violet-400">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-[#168896]">
 						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
 					</svg>
 					<div class="ml-3">
-						<dt class="text-lg font-medium">Quisque eu dui lacinia</dt>
-						<dd class="mt-2 dark:text-gray-400">Cras a mauris tincidunt, scelerisque justo sit amet, hendrerit est. Fusce venenatis diam fringilla metus convallis, at dapibus enim congue.</dd>
+						<dt class="text-lg font-medium">Doucment as you go</dt>
+						<dd class="mt-2 dark:text-gray-400">Documentation is a process, and now it's easy for accountants to get their books well-presented.</dd>
 					</div>
 				</div>
 				<div class="flex">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 dark:text-violet-400">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-[#168896]">
 						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
 					</svg>
 					<div class="ml-3">
-						<dt class="text-lg font-medium">Cras vel bibendum tellus</dt>
-						<dd class="mt-2 dark:text-gray-400">Curabitur molestie neque ac massa pulvinar, nec sollicitudin nunc consequat. Donec mattis orci eros, vitae porttitor risus pretium eget.</dd>
+						<dt class="text-lg font-medium">Open-source</dt>
+						<dd class="mt-2 dark:text-gray-400">Use the tool as the basis for your more cusomized & industry-specific tool with the least development curve.</dd>
 					</div>
 				</div>
 				<div class="flex">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 dark:text-violet-400">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-[#168896]">
 						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
 					</svg>
 					<div class="ml-3">
-						<dt class="text-lg font-medium">Dignissim magna</dt>
-						<dd class="mt-2 dark:text-gray-400">Cras ac lectus erat. Curabitur condimentum luctus nisi, non lacinia ipsum.</dd>
+						<dt class="text-lg font-medium">Free <sub>(No Fees)</sub></dt>
+						<dd class="mt-2 dark:text-gray-400">Start using this extension app as part of a whole eco-system that aims to ease ERP operators all over GCC and NA.</dd>
 					</div>
 				</div>
 			</dl>
 		</div>
+
 	</div>
     </NuxtLayout>
 </template>
-<style>.notes1{fill:#35b0b0;}.notes2{fill:#74c6c9;}.notes3{fill:#115b7c;}.notes4{fill:#168896;}
+<style scoped>.notes1{fill:#35b0b0;}.notes2{fill:#74c6c9;}.notes3{fill:#115b7c;}.notes4{fill:#168896;}
+.notes-btn{
+@apply bg-[#168896] hover:bg-[#168896]/80
+}
+
 </style>
