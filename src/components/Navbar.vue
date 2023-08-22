955861672dbb7fafc393f9babb9440bdf149ab88
TaricOv <tareq.ouf@gmail.com>
2023-08-22 22:45:35 +0300

    disabled connect btn . added descs for all prods

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -21,7 +21,8 @@ const links: LinksProps[] = [
   {
     label: "Connect",
     url: "/connect",
-    icon:""
+    icon:"",
+    disabled: true
     // <IconApi class="group-hover:animate-bounce"/>
   },
   {
@@ -47,7 +48,6 @@ const links: LinksProps[] = [
   >
     <NuxtLink to="/" class="font-bold text-5xl m-0">
       <svg class="" width="60" heigh="60" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128.65 139.81"><defs></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M94.85,5.15,0,139.81,52.44,0H64.19Q83.17,0,94.85,5.15Z"/><polygon class="cls-2" points="52.44 0 0 139.81 0 0 52.44 0"/><path class="cls-3" d="M128.65,69.43q0,22.41-5.1,34.76a57.54,57.54,0,0,1-14.16,20.7A46.32,46.32,0,0,1,89.93,136a100.26,100.26,0,0,1-25.74,3.81H0L126.83,49.42A106.53,106.53,0,0,1,128.65,69.43Z"/><path class="cls-4" d="M126.83,49.42,0,139.81,94.85,5.15a50,50,0,0,1,19.31,14.78,61.41,61.41,0,0,1,11.06,22.42C125.85,44.67,126.38,47,126.83,49.42Z"/></g></g></svg>
-
       <!-- <img
         src="https://storage.googleapis.com/lanyard/static/lanyardtemplogo.png"
         alt="logo"
@@ -56,8 +56,10 @@ const links: LinksProps[] = [
     </NuxtLink>
 
     <ul class="items-center flex flex-col sm:!space-x-3 sm:flex-row sm:space-y-0  space-y-3 text-sm sm:w-fit w-full">
-      <li v-for="l in links" :key="l.label" :class="{ 'pointer-events-none select-none cursor-default !bg-gray-500 opacity-30' : l.disabled}" class="grow !justify-center !mx-0 !w-2/3 sm:!w-fit group flex items-center sm:space-x-2 rounded px-2 py-1 bg-white/5 hover:bg-white/30 transition duration-200">
+      <li v-for="l in links" :key="l.label" :class="{ 'pointer-events-none select-none cursor-default !bg-gray-500 opacity-30' : l.disabled}" class="relative grow !justify-center !mx-0 !w-2/3 sm:!w-fit group flex items-center sm:space-x-2 rounded px-2 py-1 bg-white/5 hover:bg-white/30 transition duration-200">
         <Component :is="l.icon" class="group-hover:animate-bounce"/>
+<span class="absolute top-6 left-1 !opacity-100 text-[9px] w-fit bg-red-600 text-white rounded shdow px-[2px]" v-if="l.label === 'Connect'">Coming Soon</span>
+
         <NuxtLink :href="l.url" >{{l.label}}</NuxtLink>
       </li>
     </ul>
